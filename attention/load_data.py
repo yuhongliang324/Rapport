acc1d1af37c1c0cae7f7d784d3f092f2d1ab6407
yuhongliang324 <yuhongliang324@gmail.com>
2016-11-26 23:19:31 -0500

    trivial try

--- attention/load_data.py
+++ attention/load_data.py
@@ -9,6 +9,7 @@ n_class = 7
 def load():
     X = numpy.random.rand(2000, 50, 1000).astype(theano.config.floatX)
     X.transpose([1, 0, 2])
+    print X.shape
     y = numpy.random.randint(0, n_class, (2000,)).astype(theano.config.floatX)
     return X, y
 
