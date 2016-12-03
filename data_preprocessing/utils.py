07f86728ba8c2dd8fefaed3260dfc1f5499173d3
yuhongliang324 <yuhongliang324@gmail.com>
2016-12-03 17:42:35 -0500

    add utils

--- data_preprocessing/utils.py
+++ data_preprocessing/utils.py
@@ -1,7 +1,7 @@
 __author__ = 'yuhongliang324'
 
 import os
-
+from collections import defaultdict
 
 def rename(root):
     files = os.listdir(root)
@@ -35,7 +35,6 @@ def get_slice_ratings(rating_csv):
     lines = map(lambda x: x.strip(), lines)
 
     ret = []
-
     for line in lines:
         sp = line.split(',')
         rating = float(sp[7])
@@ -43,4 +42,30 @@ def get_slice_ratings(rating_csv):
         ret.append((dyad, session, slice, rating))
     return ret
 
+
+def get_slice_ratings2(rating_root, outfile):
+    files = os.listdir(rating_root)
+    slice_rating = defaultdict(float)
+    slice_num = defaultdict(int)
+    for fn in files:
+        if not fn.endswith('csv'):
+            continue
+        ret = get_slice_ratings(os.path.join(rating_root, fn))
+        for dyad, session, slice, rating in ret:
+            slice_name = str(dyad) + ',' + str(session) + ',' + str(slice).zfill(3)
+            slice_num[slice_name] += 1
+            slice_rating[slice_name] = \
+                (slice_rating[slice_name] * (slice_num[slice_name] - 1) + rating)/slice_num[slice_name]
+    slice_rating_sorted = sorted(slice_rating.iteritems(), key=lambda d: d[0])
+    writer = open(outfile, 'w')
+    for slice, rating in slice_rating_sorted:
+        writer.write(slice + ',' + str(rating) + '\n')
+    writer.close()
+
+
+
 data_root = '/multicomp/users/liangke/RAPT/features'
+data_info_root = '../data_info/'
+
+
+get_slice_ratings2(data_info_root, os.path.join(data_info_root, 'ratings.txt'))
