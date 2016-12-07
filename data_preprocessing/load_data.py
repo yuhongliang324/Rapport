c9bace95186aba4215887c8e284b1ccb284b285d
yuhongliang324 <yuhongliang324@gmail.com>
2016-12-06 22:43:18 -0500

    add load_data

--- data_preprocessing/load_data.py
+++ data_preprocessing/load_data.py
@@ -47,7 +47,7 @@ def load_dyad(dirname, feature_name='hog', side='b'):
             continue
         features.append(feat)
         ratings.append(rating)
-        # print feat.shape
+        print feat.shape
 
     # features = numpy.stack(features, axis=0).astype(theano.config.floatX)
     # ratings = numpy.stack(ratings, axis=0).astype(theano.config.floatX)
