81423d524ea2e14cd86777559c43dcdbd32c931e
Hongliang Yu <hongliay@atlas4.multicomp.cs.cmu.edu>
2016-12-07 01:55:16 -0500

    minor

--- attention/train.py
+++ attention/train.py
@@ -110,6 +110,9 @@ def cross_validation():
         rmse = sqrt(numpy.mean(rmse * rmse))
         print 'Testing Dyad =', dyad
         print 'RMSE of Average Prediction = %f' % rmse
+        print X_train.shape, X_test.shape
+        X_test = X_train[:X_test.shape[0]]
+        print X_test.shape
         train(X_train, y_train, X_test, y_test)
 
 
