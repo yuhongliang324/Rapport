97f7f3897377a5e44263fd7b00086872f3c29de7
yuhongliang324 <yuhongliang324@gmail.com>
2016-12-07 00:05:23 -0500

    add load_data

--- attention/train.py
+++ attention/train.py
@@ -1,16 +1,27 @@
 __author__ = 'yuhongliang324'
 
 from math import ceil
-
+import numpy
 import theano
 import theano.tensor as T
 
 from rnn_attention import RNN_Attention
+import sys
+sys.path.append('../')
 
 
-def train(X_train, y_train, X_test, y_test, hidden_dim=512, batch_size=8, num_epoch=100):
+def validate(test_model, n_test, batch_size=16):
+    num_iter = int(ceil(n_test / float(batch_size)))
+    for iter_index in xrange(num_iter):
+        start, end = iter_index * batch_size, min((iter_index + 1) * batch_size, n_test)
+        cost, loss = test_model(start, end)
+        print 'test cost', cost, 'loss', loss
+
+
+def train(X_train, y_train, X_test, y_test, hidden_dim=512, batch_size=16, num_epoch=100):
 
     n_train = X_train.shape[0]
+    n_test = X_test.shape[0]
     input_dim = X_train.shape[2]
     X_train = X_train.transpose([1, 0, 2])
     X_test = X_test.transpose([1, 0, 2])
@@ -34,13 +45,13 @@ def train(X_train, y_train, X_test, y_test, hidden_dim=512, batch_size=8, num_ep
     start_symbol, end_symbol = T.lscalar(), T.lscalar()
 
     train_model = theano.function(inputs=[start_symbol, end_symbol],
-                                  outputs=[cost], updates=updates,
+                                  outputs=[cost, loss], updates=updates,
                                   givens={
                                       X_batch: X_train_shared[:, start_symbol: end_symbol, :],
                                       y_batch: y_train_shared[start_symbol: end_symbol]},
                                   on_unused_input='ignore')
     test_model = theano.function(inputs=[start_symbol, end_symbol],
-                                  outputs=[cost], updates=updates,
+                                  outputs=[cost, loss], updates=updates,
                                   givens={
                                       X_batch: X_test_shared[:, start_symbol: end_symbol, :],
                                       y_batch: y_test_shared[start_symbol: end_symbol]},
@@ -50,13 +61,35 @@ def train(X_train, y_train, X_test, y_test, hidden_dim=512, batch_size=8, num_ep
     for epoch_index in xrange(num_epoch):
         for iter_index in xrange(num_iter):
             start, end = iter_index * batch_size, min((iter_index + 1) * batch_size, n_train)
-            cost = train_model(start, end)
+            cost, loss = train_model(start, end)
             cost = cost[0]
-            print iter_index, cost
+            print 'test cost', cost, 'loss', loss
+        validate(test_model, n_test)
+
+
+def cross_validation():
+    from data_preprocessing.load_data import load
+    from data_path import sample_10_root
+    dyad_features, dyad_ratings = load(sample_10_root)
+    dyads = dyad_features.keys()
+    num_dyad = len(dyads)
+    for i in xrange(num_dyad):
+        dyad = dyads[i]
+        X_test = dyad_features[dyad]
+        y_test = dyad_ratings[dyad]
+        feature_list, rating_list = [], []
+        for j in xrange(num_dyad):
+            if j == i:
+                continue
+            feature_list.append(dyad_features[dyads[i]])
+            rating_list.append(dyad_ratings[dyads[i]])
+        X_train = numpy.concatenate(feature_list)
+        y_train = numpy.concatenate(rating_list)
+        train(X_train, y_train, X_test, y_test)
 
 
 def test1():
-    train()
+    cross_validation()
 
 
 if __name__ == '__main__':
