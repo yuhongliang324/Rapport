feb89b98e3f37e25fed7647cadbdb3cd4a14197b
yuhongliang324 <yuhongliang324@gmail.com>
2016-12-13 15:20:04 -0500

    minor

--- data_preprocessing/load_data.py
+++ data_preprocessing/load_data.py
@@ -61,16 +61,14 @@ def load_dyad(dirname, feature_name='hog', side='b', min_step=76, norm=True):
         feat, _, rating = load_feature(mat_file, feature_name=feature_name, side=side, only_suc=False)
         if side == 'lr':
             lfeat, rfeat = feat
-            '''
             if norm:
                 lfeat = normalize(lfeat)
-                rfeat = normalize(rfeat)'''
+                rfeat = normalize(rfeat)
             prev_step = add_to_features(lfeat, rating, features, ratings, prev_step, min_step=min_step)
             prev_step = add_to_features(rfeat, rating, features, ratings, prev_step, min_step=min_step)
         else:
-            '''
             if norm:
-                feat = normalize(feat)'''
+                feat = normalize(feat)
             prev_step = add_to_features(feat, rating, features, ratings, prev_step, min_step=min_step)
     features = numpy.stack(features[:-1], axis=0).astype(theano.config.floatX)
     ratings = numpy.asarray(ratings[:-1], dtype=theano.config.floatX)
