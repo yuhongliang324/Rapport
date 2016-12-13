30ed15ff92b8c91a7c67f1d9a3940b6333fdfd4f
yuhongliang324 <yuhongliang324@gmail.com>
2016-12-13 13:47:34 -0500

    add other rnns

--- attention/rnn_attention.py
+++ attention/rnn_attention.py
@@ -12,9 +12,9 @@ from collections import OrderedDict
 class RNN_Attention(object):
     # n_class = 1: regression problem
     # n_class > 1: classification problem
-    def __init__(self, input_dim, hidden_dim, n_class, lamb=0.00001, update='rmsprop',
+    def __init__(self, input_dim, hidden_dim, n_class, rnn='naive', lamb=0.00001, update='rmsprop',
                  lr=None, beta1=0.9, beta2=0.999, epsilon=1e-8, decay=0., momentum=0.9, rho=0.9):
-
+        self.rnn = rnn
         self.input_dim, self.hidden_dim = input_dim, hidden_dim
         self.n_class = n_class
         self.lamb = lamb
@@ -25,16 +25,33 @@ class RNN_Attention(object):
         theano_seed = numpy.random.randint(2 ** 30)
         self.theano_rng = RandomStreams(theano_seed)
 
-        self.name = 'rnn_attention'
+        self.name = self.rnn + '_attention'
 
-        self.W_i, self.b_i = self.init_para(self.input_dim, self.hidden_dim)
-        self.U_i, _ = self.init_para(self.hidden_dim, self.hidden_dim)
-        self.W_f, self.b_f = self.init_para(self.input_dim, self.hidden_dim)
-        self.U_f, _ = self.init_para(self.hidden_dim, self.hidden_dim)
-        self.W_o, self.b_o = self.init_para(self.input_dim, self.hidden_dim)
-        self.U_o, _ = self.init_para(self.hidden_dim, self.hidden_dim)
-        self.W_c, self.b_c = self.init_para(self.input_dim, self.hidden_dim)
-        self.U_c, _ = self.init_para(self.hidden_dim, self.hidden_dim)
+        if self.rnn == 'lstm':
+            self.W_i, self.b_i = self.init_para(self.input_dim, self.hidden_dim)
+            self.U_i, _ = self.init_para(self.hidden_dim, self.hidden_dim)
+            self.W_f, self.b_f = self.init_para(self.input_dim, self.hidden_dim)
+            self.U_f, _ = self.init_para(self.hidden_dim, self.hidden_dim)
+            self.W_o, self.b_o = self.init_para(self.input_dim, self.hidden_dim)
+            self.U_o, _ = self.init_para(self.hidden_dim, self.hidden_dim)
+            self.W_c, self.b_c = self.init_para(self.input_dim, self.hidden_dim)
+            self.U_c, _ = self.init_para(self.hidden_dim, self.hidden_dim)
+
+            self.theta = [self.W_i, self.U_i, self.b_i, self.W_f, self.U_f, self.b_f,
+                          self.W_o, self.U_o, self.b_o, self.W_c, self.U_c, self.b_c]
+        elif self.rnn == 'gru':
+            self.W_z, self.b_z = self.init_para(self.input_dim, self.hidden_dim)
+            self.U_z, _ = self.init_para(self.hidden_dim, self.hidden_dim)
+            self.W_r, self.b_r = self.init_para(self.input_dim, self.hidden_dim)
+            self.U_r, _ = self.init_para(self.hidden_dim, self.hidden_dim)
+            self.W_h, self.b_h = self.init_para(self.input_dim, self.hidden_dim)
+            self.U_h, _ = self.init_para(self.hidden_dim, self.hidden_dim)
+            self.theta = [self.W_z, self.U_z, self.b_z, self.W_r, self.U_r, self.b_r,
+                          self.W_h, self.U_h, self.b_h]
+        else:
+            self.W_left, self.b_left = self.init_para(self.input_dim, self.hidden_dim)
+            self.U_left, _ = self.init_para(self.hidden_dim, self.hidden_dim)
+            self.theta = [self.W_left, self.U_left, self.b_left]
 
         self.W_s, self.b_s = self.init_para(self.input_dim, self.hidden_dim)
         self.U_s, _ = self.init_para(self.hidden_dim, self.hidden_dim)
@@ -43,13 +60,10 @@ class RNN_Attention(object):
             low=-numpy.sqrt(6. / float(self.hidden_dim + 1)), high=numpy.sqrt(6. / float(self.hidden_dim + 1)),
             size=(self.hidden_dim,)), dtype=theano.config.floatX)
         self.w_a = theano.shared(value=w_a, borrow=True)
-        # self.b_a = theano.shared(value=0., borrow=True)
 
         self.W_1, self.b_1 = self.init_para(self.hidden_dim, self.n_class)
 
-        self.theta = [self.W_i, self.U_i, self.b_i, self.W_f, self.U_f, self.b_f,
-                      self.W_o, self.U_o, self.b_o, self.W_c, self.U_c, self.b_c,
-                      self.w_a, self.W_1, self.b_1, self.W_s, self.U_s, self.b_s]
+        self.theta += [self.w_a, self.W_1, self.b_1, self.W_s, self.U_s, self.b_s]
 
         self.add_param_shapes()
 
@@ -122,7 +136,7 @@ class RNN_Attention(object):
     # scan function parameter order: sequences, prior results, non_sequences
     # sequences: X_t (batch_size, input_dim)
     # prior results: C_tm1, H_tm1, S_tm1 (batch_size, hidden_dim)
-    def forward(self, X_t, C_tm1, H_tm1, S_tm1):
+    def forward_LSTM(self, X_t, C_tm1, H_tm1, S_tm1):
         i_t = T.nnet.sigmoid(T.dot(X_t, self.W_i) + T.dot(H_tm1, self.U_i) + self.b_i)  # (batch_size, hidden_dim)
         f_t = T.nnet.sigmoid(T.dot(X_t, self.W_f) + T.dot(H_tm1, self.U_f) + self.b_f)  # (batch_size, hidden_dim)
         o_t = T.nnet.sigmoid(T.dot(X_t, self.W_o) + T.dot(H_tm1, self.U_o) + self.b_o)  # (batch_size, hidden_dim)
@@ -133,6 +147,23 @@ class RNN_Attention(object):
         S_t = T.tanh(T.dot(X_t, self.W_s) + T.dot(S_tm1, self.U_s) + self.b_s)  # (batch_size, hidden_dim)
         S_t = ((1. - a_t) * S_tm1.T + a_t * S_t.T).T
         return a_t, C_t, H_t, S_t
+
+    def forward_GRU(self, X_t, H_tm1, S_tm1):
+        z_t = T.nnet.sigmoid(T.dot(X_t, self.W_z) + T.dot(H_tm1, self.U_z) + self.b_z)
+        r_t = T.nnet.sigmoid(T.dot(X_t, self.W_r) + T.dot(H_tm1, self.U_r) + self.b_r)
+        H_t = T.tanh(T.dot(X_t, self.W_h) + T.dot(r_t * H_tm1, self.U_h) + self.b_h)
+        H_t = (1 - z_t) * H_tm1 + z_t * H_t
+        a_t = T.nnet.sigmoid(T.dot(H_t, self.w_a))
+        S_t = T.tanh(T.dot(X_t, self.W_s) + T.dot(S_tm1, self.U_s) + self.b_s)
+        S_t = ((1. - a_t) * S_tm1.T + a_t * S_t.T).T
+        return a_t, H_t, S_t
+
+    def forward_naive(self, X_t, H_tm1, S_tm1):
+        H_t = T.nnet.softplus(T.dot(X_t, self.W_left) + T.dot(H_tm1, self.U_left) + self.b_left)
+        a_t = T.nnet.sigmoid(T.dot(H_t, self.w_a))
+        S_t = T.tanh(T.dot(X_t, self.W_s) + T.dot(S_tm1, self.U_s) + self.b_s)
+        S_t = ((1. - a_t) * S_tm1.T + a_t * S_t.T).T
+        return a_t, H_t, S_t
 
     def build_model(self):
         X_batch = T.tensor3()  # (n_step, batch_size, input_dim)
@@ -146,11 +177,23 @@ class RNN_Attention(object):
         # a: (n_step, batch_size)
         # C: (n_step, batch_size, hidden_dim)
         # H: (n_step, batch_size, hidden_dim)
-        [a, _, _, S], _ = theano.scan(self.forward, sequences=X_batch,
-                                   outputs_info=[None,
-                                                 T.zeros((batch_size, self.hidden_dim), dtype=theano.config.floatX),
-                                                 T.zeros((batch_size, self.hidden_dim), dtype=theano.config.floatX),
-                                                 T.zeros((batch_size, self.hidden_dim), dtype=theano.config.floatX)])
+
+        if self.rnn == 'lstm':
+            [a, _, _, S], _ = theano.scan(self.forward_LSTM, sequences=X_batch,
+                                          outputs_info=[None,
+                                                        T.zeros((batch_size, self.hidden_dim), dtype=theano.config.floatX),
+                                                        T.zeros((batch_size, self.hidden_dim), dtype=theano.config.floatX),
+                                                        T.zeros((batch_size, self.hidden_dim), dtype=theano.config.floatX)])
+        elif self.rnn == 'gru':
+            [a, _, S], _ = theano.scan(self.forward_GRU, sequences=X_batch,
+                                       outputs_info=[None,
+                                                     T.zeros((batch_size, self.hidden_dim), dtype=theano.config.floatX),
+                                                     T.zeros((batch_size, self.hidden_dim), dtype=theano.config.floatX)])
+        else:
+            [a, _, S], _ = theano.scan(self.forward_naive, sequences=X_batch,
+                                       outputs_info=[None,
+                                                     T.zeros((batch_size, self.hidden_dim), dtype=theano.config.floatX),
+                                                     T.zeros((batch_size, self.hidden_dim), dtype=theano.config.floatX)])
 
         rep = S[-1]  # (batch_size, hidden_dim)
         rep = T.dot(rep, self.W_1) + self.b_1  # (batch_size, n_class)
