d1233680fae27ebe6a5fd04554e33afa01ace06f
yuhongliang324 <yuhongliang324@gmail.com>
2016-12-13 14:13:23 -0500

    add lr

--- data_preprocessing/load_data.py
+++ data_preprocessing/load_data.py
@@ -64,7 +64,6 @@ def load_dyad(dirname, feature_name='hog', side='b', min_step=76):
             prev_step = add_to_features(rfeat, rating, features, ratings, prev_step, min_step=min_step)
         else:
             prev_step = add_to_features(feat, rating, features, ratings, prev_step, min_step=min_step)
-    print [x.shape for x in features[:-1]]
     features = numpy.stack(features[:-1], axis=0).astype(theano.config.floatX)
     ratings = numpy.asarray(ratings[:-1], dtype=theano.config.floatX)
 
