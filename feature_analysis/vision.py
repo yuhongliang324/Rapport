01ecdf6e72542436bd267ded42e0eef1110e4194
yuhongliang324 <yuhongliang324@gmail.com>
2017-01-18 19:43:53 -0500

    add feature analysis

--- feature_analysis/vision.py
+++ feature_analysis/vision.py
@@ -76,7 +76,7 @@ def get_PCC():
         print name
         print '\t'
         for j in xrange(topK):
-            print gemo_names[ind[j]], PCC_abs[ind[j]] * PCC_sign[ind[j]] + ';',
+            print '%s %.2f;' % (gemo_names[ind[j]], PCC_abs[ind[j]] * PCC_sign[ind[j]]),
 
     PCC(X_mean, y, 'mean')
     PCC(X_max, y, 'max')
