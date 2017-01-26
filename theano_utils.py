4dec5dd05d8f772fe6bb78bae471b5bc8c8db6fe
yuhongliang324 <yuhongliang324@gmail.com>
2017-01-25 19:32:15 -0500

    change optimizer

--- theano_utils.py
+++ theano_utils.py
@@ -5,10 +5,29 @@ from theano.tensor.shared_randomstreams import RandomStreams
 import numpy
 
 
+def Adam2(params, cost, lr=0.0001, b1=0.1, b2=0.1, e=1e-8):
+    """
+    no bias init correction
+    """
+    updates = []
+    grads = T.grad(cost, params)
+    for p, g in zip(params, grads):
+        m = theano.shared(p.get_value() * 0.)
+        v = theano.shared(p.get_value() * 0.)
+        m_t = (b1 * g) + ((1. - b1) * m)
+        v_t = (b2 * T.sqr(g)) + ((1. - b2) * v)
+        g_t = m_t / (T.sqrt(v_t) + e)
+        p_t = p - (lr * g_t)
+        updates.append((m, m_t))
+        updates.append((v, v_t))
+        updates.append((p, p_t))
+    return updates
+
+
 def Adam(cost, params, lr=0.0002, b1=0.1, b2=0.001, e=1e-8):
     updates = []
     grads = T.grad(cost, params)
-    i = theano.shared(theano.config.floatX(0.))
+    i = theano.shared(0.)
     i_t = i + 1.
     fix1 = 1. - (1. - b1)**i_t
     fix2 = 1. - (1. - b2)**i_t
