2738934bb4fff0a6a7307d9258dc55e5ecf1086a
yuhongliang324 <yuhongliang324@gmail.com>
2017-01-25 19:38:51 -0500

    change optimizer

--- theano_utils.py
+++ theano_utils.py
@@ -5,7 +5,7 @@ from theano.tensor.shared_randomstreams import RandomStreams
 import numpy
 
 
-def Adam(params, cost, lr=0.0001, b1=0.1, b2=0.1, e=1e-8):
+def Adam(cost, params, lr=0.0001, b1=0.1, b2=0.1, e=1e-8):
     """
     no bias init correction
     """
