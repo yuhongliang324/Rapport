29d0f49efd7a60398242e08ffee425c39e4e032e
yuhongliang324 <yuhongliang324@gmail.com>
2017-02-01 20:26:56 -0500

    change plot

--- attention/model_utils.py
+++ attention/model_utils.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 def plot_loss(img_path, costs_train, costs_val, dyad, losses_krip_train=None, losses_krip_val=None):
     plt.figure(figsize=(10, 8))
     plt.plot(costs_train, label='Training Loss')
-    plt.plot(costs_val, label='Validation Loss')
+    plt.plot(costs_val, '--', label='Validation Loss')
     if losses_krip_train is not None:
-        plt.plot(losses_krip_train, '--', label='Training Krip Loss')
+        plt.plot(losses_krip_train, label='Training Krip Loss')
         plt.plot(losses_krip_val, '--', label='Validation Krip Loss')
     plt.legend()
     plt.title('Dyad ' + str(dyad))
