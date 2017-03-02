a39fc8f0472cdedd445b1c315c0eda0a4faad71a
yuhongliang324 <yuhongliang324@gmail.com>
2017-03-02 01:29:10 -0500

    add train ptb

--- PTB/train.py
+++ PTB/train.py
@@ -103,7 +103,7 @@ def train(drop=0., hidden_dim=None, batch_size=32, num_epoch=50):
 
 def test1():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-hid', type=int, default=256)
+    parser.add_argument('-hid', type=int, default=100)
     parser.add_argument('-drop', type=float, default=0.)
     parser.add_argument('-epoch', type=int, default=50)
     args = parser.parse_args()
