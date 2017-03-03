bf885686287292d2f148071913dfa43a4872cd7d
yuhongliang324 <yuhongliang324@gmail.com>
2017-03-02 19:51:38 -0500

    modify adam

--- theano_utils.py
+++ theano_utils.py
@@ -24,7 +24,7 @@ def Adam(cost, params, lr=0.0001, b1=0.1, b2=0.1, e=1e-8):
     return updates
 
 
-def Adam2(cost, params, lr=0.001, b1=0.1, b2=0.1, e=1e-8):
+def Adam2(cost, params, lr=0.001, b1=0.9, b2=0.999, e=1e-8):
     """
     no bias init correction
     """
@@ -33,10 +33,10 @@ def Adam2(cost, params, lr=0.001, b1=0.1, b2=0.1, e=1e-8):
     for p, g in zip(params, grads):
         m = theano.shared(p.get_value() * 0.)
         v = theano.shared(p.get_value() * 0.)
-        m_t = (b1 * g) + ((1. - b1) * m)
-        v_t = (b2 * T.sqr(g)) + ((1. - b2) * v)
+        m_t = b1 * m + (1. - b1) * g
+        v_t = b2 * v + (1. - b2) * T.sqr(g)
         g_t = m_t / (T.sqrt(v_t) + e)
-        p_t = p - (lr * g_t)
+        p_t = p - lr * g_t
         updates.append((m, m_t))
         updates.append((v, v_t))
         updates.append((p, p_t))
