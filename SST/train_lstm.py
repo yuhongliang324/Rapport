c24dd5fc3acf677e94e3ab980d0d747affc7182d
yuhongliang324 <yuhongliang324@gmail.com>
2017-03-03 13:47:39 -0500

    change mlp

--- SST/train_lstm.py
+++ SST/train_lstm.py
@@ -20,7 +20,7 @@ def train(drop=0., hidden_dim=None, lamb=0.0001, bidirection=False, update='adam
     X_batches_val = [Xb.transpose([1, 0, 2]) for Xb in X_batches_val]
     X_batches_test = [Xb.transpose([1, 0, 2]) for Xb in X_batches_test]
 
-    model = LSTM(input_dim, hidden_dim, [num_class], drop=drop, lamb=lamb, bidirection=bidirection, update=update)
+    model = LSTM(input_dim, hidden_dim, [hidden_dim, num_class], drop=drop, lamb=lamb, bidirection=bidirection, update=update)
     symbols = model.build_model()
 
     X_batch, y_batch, is_train = symbols['X_batch'], symbols['y_batch'], symbols['is_train']
