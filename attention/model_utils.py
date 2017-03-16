019103ba6f11dd2287084d4df5c8c9f4c0782063
yuhongliang324 <yuhongliang324@gmail.com>
2017-03-16 00:09:00 -0400

    add category

--- attention/model_utils.py
+++ attention/model_utils.py
@@ -7,13 +7,13 @@ import matplotlib.pyplot as plt
 def plot_loss(img_path, dyad, costs_train, costs_val, losses_krip_train=None, losses_krip_val=None,
               costs_test=None, losses_krip_test=None, tdyad=None):
     plt.figure(figsize=(10, 8))
-    plt.plot(costs_train, label='Training Loss')
-    plt.plot(costs_val, '--', label='Validation Loss')
+    plt.plot(costs_train, 'b-', label='Training Loss')
+    plt.plot(costs_val, 'm--', label='Validation Loss')
     if losses_krip_train is not None:
         plt.plot(losses_krip_train, label='Training Krip Loss')
         plt.plot(losses_krip_val, '--', label='Validation Krip Loss')
     if costs_test is not None:
-        plt.plot(costs_test, label='Test Loss')
+        plt.plot(costs_test, 'c--', label='Test Loss')
     if losses_krip_test is not None:
         plt.plot(losses_krip_test, label='Test Krip Loss')
     plt.legend()
