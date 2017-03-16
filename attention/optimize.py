240bff71ff36a4500b2aa36427e612eab92247aa
yuhongliang324 <yuhongliang324@gmail.com>
2017-03-15 22:40:56 -0400

    add category

--- attention/optimize.py
+++ attention/optimize.py
@@ -140,12 +140,14 @@ def train(X_train, y_train, X_val, y_val, X_test, y_test, drop=0.25, final_activ
             loss_krip_avg /= n_train
             losses_krip_train.append(loss_krip_avg)
         y_predicted = numpy.asarray(all_pred)
+        print y_predicted
+        print y_train.shape, y_predicted.shape
         rmse_acc = eval(y_train, y_predicted)
         if category:
             print '\tTrain cost = %f,\tAccuracy = %f' % (cost_avg, rmse_acc)
         else:
             print '\tTrain cost = %f,\tKrip Loss = %f,\tRMSE = %f' % (cost_avg, loss_krip_avg, rmse_acc)
-        cost_avg_val, _ = validate(valid_model, y_val, costs_val, losses_krip_val)
+        cost_avg_val, _ = validate(valid_model, y_val, costs_val, losses_krip_val, category=category)
         _, pred_test = validate(test_model, y_test, costs_test, losses_krip_test)
         if cost_avg_val < best_cost_val:
             best_cost_val = cost_avg_val
