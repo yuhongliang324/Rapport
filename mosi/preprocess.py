8aa84e400d819ea323c1b41231cf4de18bb075b8
yuhongliang324 <yuhongliang324@gmail.com>
2017-03-27 16:13:35 -0400

    add preprocess mosi

--- mosi/preprocess.py
+++ mosi/preprocess.py
@@ -54,8 +54,8 @@ def get_facet_features(video_range):
         video_path = os.path.join(raw_facet_root, csv)
         features = numpy.loadtxt(video_path, skiprows=1, delimiter=',')
         segID_cont = video_range[videoID]
+        print segID_cont.keys()
         for segID, cont in segID_cont.items():
-            print cont.keys()
             start_time, end_time = cont['start_time'], cont['end_time']
             start_frame, end_frame = int(start_time * 30), int(end_time * 30)
             feat = features[start_frame: end_frame + 1]
