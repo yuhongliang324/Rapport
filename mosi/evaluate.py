38e4615b0518c59beb5eaee64c0a1227dd6b0b3d
yuhongliang324 <yuhongliang324@gmail.com>
2017-04-16 10:39:40 -0400

    add pearson

---
+++ mosi/evaluate.py
@@ -0,0 +1,16 @@
+__author__ = 'yuhongliang324'
+
+import numpy
+from scipy.stats import pearsonr
+import sys
+
+
+def evaluate(fn):
+    P = numpy.loadtxt(fn, delimiter=',')
+    pred = P[:, 0]
+    gt = P[:, 1]
+    r = pearsonr(pred, gt)[0]
+    return r
+
+
+evaluate(sys.argv[1])
